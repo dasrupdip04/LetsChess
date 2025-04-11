4aec21fff94efad2c21412c46521fd5cb11165ba
dasrupdip04 <dasrupdip04@gmail.com>
2025-04-11 19:54:26 +0530

    update chat.py and .env

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -1,26 +1,18 @@
-from google import generativeai as genai
+from google import genai
+from google.genai import types
 import os
 from dotenv import load_dotenv
 
-# Load API key from .env
+
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
-# Configure the genai client
-genai.configure(api_key=api_key)
-
-# Load the model
-model = genai.GenerativeModel("gemini-1.5-flash")
 
 
-# Terminal loop
-print("🤖 GeminiGPT is online. Type 'exit' to quit.\n")
-
-while True:
-    prompt = input("You: ")
-    if prompt.lower() in ["exit", "quit"]:
-        print("GeminiGPT: Bye!")
-        break
-
-    response = model.generate_content(prompt)
-    print(f"GeminiGPT: {response.text}\n")
+client = genai.Client(api_key=api_key)
+response= client.models.generate_content(
+    model="gemini-1.5-flash",
+    contents="What is life",
+    
+)
+print(response.text)
